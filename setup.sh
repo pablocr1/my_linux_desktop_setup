f2630bc851d37a2418006882a38b6c849c2c6c85
Pablo Carvalho <pablocarvalho.dev@gmail.com>
2022-06-16 08:49:28 -0300

    Fix vscode, chrome and add beekeeper

--- setup.sh
+++ setup.sh
@@ -8,57 +8,54 @@ read -p "Pressione <ENTER> para continuar." pass
 
 echo "Atualizando o sistema..."
 sudo apt update && sudo apt upgrade -y
-clear
 
 echo "Instalando algumas dependências..."
 sudo apt install software-properties-common apt-transport-https wget curl -y
+sudo apt install openjdk-8-jdk openjdk-11-jdk -y
 
 echo "Instalando o python 3..."
 sudo apt-get install python3 python3-venv python3-pip -y
 
 echo "Instalando o Chrome..."
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
 rm google-chrome-stable_current_amd64.deb
 
 echo "Instalando o Visual Studio Code..."
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add –
 sudo add-apt-repository “deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main”
-sudo apt install code -y
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+
+echo "Instalando o Beekeeper..."
+wget --quiet -O - https://deb.beekeeperstudio.io/beekeeper.key | sudo apt-key add -
+echo "deb https://deb.beekeeperstudio.io stable main" | sudo tee /etc/apt/sources.list.d/beekeeper-studio-app.list
+sudo apt update
+sudo apt install beekeeper-studio
 
 echo "Baixando o JetBrains Toolbox (instalador do Pycharm e Android Studio)..."
 wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.24.12080.tar.gz\?_gl\=1\*11v8mpp\*_ga\*NTQ5NTIxOTgzLjE2NTQxMjEwNDk.\*_ga_9J976DJZ68\*MTY1NDI1NTEyOC4yLjEuMTY1NDI1NTE1NS4w\&_ga\=2.82006932.32517002.1654255129-549521983.1654121049
 echo "Extraia o arquivo do JetBrains Toolbox e execute o appimage"
 read -p "Pressione enter para continuar" pass
 
-echo "Baixando o WhatsApp Web..."
-https://www.thefanclub.co.za/sites/all/modules/pubdlcnt/pubdlcnt.php?file=https://www.thefanclub.co.za/sites/default/files/public/downloads/whatsapp-webapp_1.0_all.deb&nid=200
-sudo dpkg -i whatsapp-webapp_1.0_all.deb
-rm whatsapp-webapp_1.0_all.deb
-
 echo "Instalando o git..."
 sudo apt install git -y
-clear
 
 echo "Instalando o postman..."
 snap install --channel=v7 postman
-clear
 
 echo "Instalando o spotify..."
 curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install spotify-client -y
-clear
 
 echo "Instalando o nodejs..."   
 curl -sL https://deb.nodesource.com/setup_14.x -o setup_14.sh
 sudo sh ./setup_14.sh
 sudo apt install -y nodejs
-clear
 
 echo "Instalando o yarn..." 
 sudo npm install -g yarn
-clear
 
 echo "Instalando o docker..."
 curl -fsSL https://get.docker.com -o get-docker.sh
@@ -69,35 +66,34 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo usermod -aG docker $USER
-clear
 
 sudo rm get-docker.sh
 
 sudo rm setup_14.sh
 
-echo "Salvando as váriaveis de ambiente para o BASH..."
-echo "alias dc=\"docker-compose\"" >> .bashrc
-echo "export ANDROID_HOME=$HOME/Android" >> .bashrc
-echo "export ANDROID_HOME=$HOME/Android/Sdk" >> .bashrc
-echo "export PATH=$PATH:$ANDROID_HOME/tools" >> .bashrc
-echo "export ANDROID_SDK_ROOT=$HOME/Android/Sdk" >> .bashrc
-echo "export PATH=$PATH:$ANDROID_HOME/emulator" >> .bashrc
-echo "export ANDROID_AVD_HOME=.android/avd" >> .bashrc
-echo "export PATH=$PATH:$HOME/bin:$ANDROID_SDK_ROOT/tools" >> .bashrc
-echo "export PATH=${PATH}:${ANDROID_SDK_ROOT}/tools" >> .bashrc
-echo "export PATH=$PATH:$ANDROID_HOME/tools/bin" >> .bashrc
-echo "export PATH=${PATH}:${ANDROID_SDK_ROOT}/platform-tools" >> .bashrc
+# echo "Salvando as váriaveis de ambiente para o BASH..."
+# echo "alias dc=\"docker-compose\"" >> .bashrc
+# echo "export ANDROID_HOME=$HOME/Android" >> .bashrc
+# echo "export ANDROID_HOME=$HOME/Android/Sdk" >> .bashrc
+# echo "export PATH=$PATH:$ANDROID_HOME/tools" >> .bashrc
+# echo "export ANDROID_SDK_ROOT=$HOME/Android/Sdk" >> .bashrc
+# echo "export PATH=$PATH:$ANDROID_HOME/emulator" >> .bashrc
+# echo "export ANDROID_AVD_HOME=.android/avd" >> .bashrc
+# echo "export PATH=$PATH:$HOME/bin:$ANDROID_SDK_ROOT/tools" >> .bashrc
+# echo "export PATH=${PATH}:${ANDROID_SDK_ROOT}/tools" >> .bashrc
+# echo "export PATH=$PATH:$ANDROID_HOME/tools/bin" >> .bashrc
+# echo "export PATH=${PATH}:${ANDROID_SDK_ROOT}/platform-tools" >> .bashrc
 
 # Os comandos a seguir devem ser executados na pasta HOME do usuário
 cd $HOME
 
-mkdir .icons
+# mkdir .icons
 
-echo "Instalando o tema Dracula para o sistema..."
-wget https://github.com/dracula/gtk/files/5214870/Dracula.zip
-unzip Dracula.zip
-mv Dracula ~/.icons/
-gsettings set org.gnome.desktop.interface icon-theme "Dracula"
+# echo "Instalando o tema Dracula para o sistema..."
+# wget https://github.com/dracula/gtk/files/5214870/Dracula.zip
+# unzip Dracula.zip
+# mv Dracula ~/.icons/
+# gsettings set org.gnome.desktop.interface icon-theme "Dracula"
 
 echo "Gerando chave SSH para o git..."
 read -p "Digite seu email utilizado no Github: " email
